6c874954ddecafb649ca8e891fa964b3158f0cde
John Paxton <pax@speedingplanet.com>
2019-11-14 09:41:09 -0600

    Redux updates

--- src/first-redux.js
+++ src/first-redux.js
@@ -6,21 +6,45 @@ const initialState = {
 
 const actionTypes = {
   INCREMENT: 'INCREMENT',
-  DECREMENT: 'DECREMENT',
   ADD: 'ADD',
-  SUBTRACT: 'SUBTRACT',
-}
+  SET_COUNTER: 'SET_COUNTER',
+};
 
+// Action Creator
 const increment = () => {
   return {
-    type: actionTypes.INCREMENT
-  }
-}
+    type: actionTypes.INCREMENT,
+  };
+};
 
-const reducer = (state, action) => {
-  switch (action.type) {
+const add = amount => {
+  return {
+    type: actionTypes.ADD,
+    payload: {
+      amount,
+    },
+  };
+};
+
+const setCounter = counter => {
+  return {
+    type: actionTypes.SET_COUNTER,
+    payload: {
+      counter,
+    },
+  };
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
     case actionTypes.INCREMENT:
       return { ...state, counter: state.counter + 1 };
+    case actionTypes.ADD:
+      return { ...state, counter: state.counter + payload.amount };
+    case actionTypes.SET_COUNTER:
+      // return {...state, counter: payload.counter};
+      return {...state, ...payload};
+      // return Object.assign({}, state, payload);
     default:
       return state;
   }
@@ -31,6 +55,9 @@ store.subscribe(() => {
   console.log('Store state: ', store.getState());
 });
 store.dispatch(increment());
+store.dispatch({ type: 'GREETING' });
 store.dispatch(increment());
 store.dispatch(increment());
 store.dispatch(increment());
+store.dispatch(add(10));
+store.dispatch(setCounter(12));
