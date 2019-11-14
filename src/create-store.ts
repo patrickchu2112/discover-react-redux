1835b31aa586bd4146f4af2bc11f4eec798a2d8a
John Paxton <pax@speedingplanet.com>
2019-11-14 12:41:30 -0600

    lunch

--- src/create-store.ts
+++ src/create-store.ts
@@ -1,11 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as demosReducer, DemosState } from './demos/demos-reducer';
 
 const initialState: DemosState = {
-  counter: 5
-}
+  counter: 5,
+};
 
-const store = createStore(demosReducer, initialState);
+const store = createStore(
+  demosReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(logger)),
+);
 
-export {store};
-
+export { store };
